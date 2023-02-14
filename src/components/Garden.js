b77d9578d4dd9c34f8c0aa75925334eb751a47d9
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-02-14 11:36:13 -0500

    lay out 100 x 50 square grid to track clicks

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -3,10 +3,23 @@ import ThePainting from '../img/garden.jpg';
 import '../styles/garden.scss';
 
 function Garden() {
+
+    // the click grid will contain 5000 squares (50 squares in the y direction, 100 in the x)
+    let gridSquares = [];
+    for (let i = 0; i < 5000; i++) {
+        gridSquares.push(
+            <div 
+                className={`grid-square`}
+                data-id={i} 
+                key={i}>    
+            </div>
+        );
+    }
+
     return (
         <div className="garden">
             <div className='grid'>
-                
+                {gridSquares}
             </div>
             <div className='painting-container'>
                 <img 
