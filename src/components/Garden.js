665c13367f5b2a09b4230b7227978d49a91c14af
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-02-14 12:15:49 -0500

    get scrolling function working

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -1,8 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
 import ThePainting from '../img/garden.jpg';
 
 import '../styles/garden.scss';
 
 function Garden() {
+
+    // state:
+    const [mousePosition, setMousePosition] = useState({
+        x: 0,
+        y: 0
+    });
+
+    // hooks:
+    useEffect(() => {
+        const scrollAmount = 100;
+        // scroll right conditions:
+        if ((mousePosition.x / window.innerWidth) > 0.875) {
+            window.scrollBy(scrollAmount, 0);
+        }
+        // scroll down:
+        if ((mousePosition.y / window.innerHeight) > 0.875) {
+            window.scrollBy(0, scrollAmount);
+        }
+        // scroll left:
+        if ((mousePosition.x / window.innerWidth) < 0.125) {
+            window.scrollBy(-1*scrollAmount, 0);
+        }
+        // scroll up:
+        if ((mousePosition.y / window.innerHeight) < 0.125) {
+            window.scrollBy(0, -1*scrollAmount);
+        }
+    }, [mousePosition]);
+
+    function handleMouseMove(e) {
+        setMousePosition({
+            x: e.clientX,
+            y: e.clientY
+        });
+    }
 
     // the click grid will contain 5000 squares (50 squares in the y direction, 100 in the x)
     let gridSquares = [];
@@ -17,7 +53,10 @@ function Garden() {
     }
 
     return (
-        <div className="garden">
+        <div 
+            className="garden"
+            onMouseMove={(e) => handleMouseMove(e)}
+        >
             <div className='grid'>
                 {gridSquares}
             </div>
